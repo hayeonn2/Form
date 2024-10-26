35c767a669f615d3b094b2e75667ada1fe7beaf0
hayoenn2 <dwd1205@naver.com>
2024-10-26 15:46:31 +0900

    feat: react-dom 에서 useFormState import 해야 함 (useFormState 오류 해결)

--- src/app/api/actions.js
+++ src/app/api/actions.js
@@ -1,11 +1,12 @@
 "use server"
 
-export async function textFormAction({success, msg}, formData) {
+export async function textFormAction(message, formData) {
     try {
-        console.log(formData, "폼?ㅎㅎ");
-        return { success: true, msg: "성공적으로 처리되었습니다." };
+        console.log(formData.get("text"), "폼?ㅎㅎ");
+
+        return { message: "성공적으로 처리되었습니다." };
     } catch (e) {
         console.error("에러 발생:", e);
-        return { success: false, msg: e.message || "알 수 없는 에러 발생" };
+        return { message: "알 수 없는 에러 발생" };
     }
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,35 +1,33 @@
 "use client"
 
-import {useForm, useFormState} from "react-hook-form";
-import {useState} from "react";
+import {useForm} from "react-hook-form";
+import {useFormState} from "react-dom";
+import {useEffect, useState} from "react";
 import {textFormAction} from "@/app/api/actions";
 
-export default function Home() {
-    const initFormState = { success: false, msg: "" };
-    const [actionState, formAction] = useFormState(textFormAction,
-        initFormState);
-    const [text, setText] = useState("");
 
+export default function Home() {
+    const [actionState, formAction] = useFormState(textFormAction, {message: ""});
+    const [text, setText] = useState("");
 
     const form = useForm({
         mode: "onChange"
     });
+
 
     function handleInputChange(e) {
         setText(e.target.value);
     }
 
     useEffect(() => {
-        if (actionState?.msg && !actionState?.success) return console.error(actionState.msg);
-        // if (actionState.success) router.replace("/");
+        if (actionState?.message) return console.error(actionState.message);
     }, [actionState]);
 
     return (
     <form action={formAction} className="border w-[500px] bg-[#ddd] rounded-[10px] mx-auto mt-[120px]">
-      <input className={"text-black"} type={"text"} {...form.register("test", {
+      <input className={"text-black"} type={"text"} {...form.register("text", {
           onChange: (e) => handleInputChange(e)
       })} />
-
         <button className={"p-[5px] bg-sky-800"} type={"submit"}>제출버튼!</button>
 
         <p className="text-black mt-[30px]">결과값: {text}</p>
