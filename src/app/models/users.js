286cfa12f64a7675649f06da3f00904504905088
hayoenn2 <dwd1205@naver.com>
2024-10-29 21:48:34 +0900

    feat: .prettierrc setting 및 Home 페이지 수정

--- src/app/models/users.js
+++ src/app/models/users.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-    userId: { type: String, required: true },
-    userPw: { type: String, required: true },
-    name: String,
-    // createDate: { type: Date, default: Date.now },
+  userId: { type: String, required: true },
+  userPw: { type: String, required: true },
+  name: String,
+  // createDate: { type: Date, default: Date.now },
 });
 
 // 모델을 정의하고, 이미 존재하는 경우 재사용
